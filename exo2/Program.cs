29fd036286d81f4ebd12d3b9c7e3a4925789e0e8
Prince@ <61355057+Prince-Isasak@users.noreply.github.com>
2020-03-03 17:03:58 +0000

    my first commit

---
+++ exo2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace exo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var NombreMaqique = 5;
+                Console.WriteLine("entrer un nombre");
+                var Uitilsateur = Console.ReadLine();
+                var user = Convert.ToInt32(Uitilsateur);
+
+
+                if (user > NombreMaqique)
+                {
+                    Console.WriteLine("Ce nombre est trop grand");
+                }
+                else if (user < NombreMaqique)
+                {
+                    Console.WriteLine("Ce nombre est trop petit");
+                }
+                else
+                {
+                    Console.WriteLine("Bravo vous avez trouvé le nombre magique");
+                }
+            }
+            catch 
+            {
+                Console.WriteLine("entrer un nombre svp");
+
+            }
+
+            
+        }
+    }
+}
